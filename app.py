ce7ebf0234647dc52b88519e1c82176077dbd783
Anshul Gupta <anshul.dlf@gmail.com>
2015-12-18 02:25:44 -0500

    pushing stuff for the maps

--- app.py
+++ app.py
@@ -17,6 +17,22 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/explore')
+def explore():
+    return render_template('explore.html')
+
+@app.route('/exploreitaly')
+def exploreitaly():
+    return render_template('exploreitaly.html')
+
+@app.route('/exploreusa')
+def exploreusa():
+    return render_template('exploreusa.html')
+
+@app.route('/exploretaiwan')
+def exploretaiwan():
+    return render_template('exploretaiwan.html')
+
 if __name__ == '__main__':
     app.run(debug = True, host='0.0.0.0', port=5000)
 
